3797b02f8ca9c881877eb761ca6bac4302c333a9
Lynn Garren <garren@fnal.gov>
2016-06-28 12:20:59 -0500

    remove unnecessary template instantiations

--- lardataobj/MCBase/classes.h
+++ lardataobj/MCBase/classes.h
@@ -10,30 +10,3 @@
 #include "lardataobj/MCBase/MCHitCollection.h"
 #include "lardataobj/MCBase/MCShower.h"
 #include "lardataobj/MCBase/MCTrack.h"
-
-template class art::Wrapper< sim::MCHit >;
-template class art::Wrapper< sim::MCWire   >;
-template class art::Wrapper< sim::MCWireCollection >;
-template class art::Wrapper< sim::MCHitCollection  >;
-
-template class std::vector< sim::MCHit >;
-template class std::vector< sim::MCWire   >;
-template class std::vector< sim::MCWireCollection >;
-template class std::vector< sim::MCHitCollection  >;
-
-template class art::Wrapper< std::vector<sim::MCHit> >;
-template class art::Wrapper< std::vector< ::sim::MCWire   > >;
-template class art::Wrapper< std::vector< ::sim::MCWireCollection > >;
-template class art::Wrapper< std::vector< ::sim::MCHitCollection  > >;
-
-template class art::Wrapper< sim::MCShower >;
-template class std::vector< sim::MCShower >;
-template class art::Wrapper< std::vector< ::sim::MCShower > >;
-
-template class art::Wrapper< sim::MCStep >;
-template class std::vector< sim::MCStep >;
-template class art::Wrapper< std::vector< ::sim::MCStep > >;
-
-template class art::Wrapper< sim::MCTrack >;
-template class std::vector< sim::MCTrack >;
-template class art::Wrapper< std::vector< ::sim::MCTrack > >;
--- lardataobj/OpticalDetectorData/classes.h
+++ lardataobj/OpticalDetectorData/classes.h
@@ -8,17 +8,3 @@
 #include "lardataobj/OpticalDetectorData/FIFOChannel.h"
 #include "lardataobj/OpticalDetectorData/PMTTrigger.h"
 #include "lardataobj/OpticalDetectorData/OpticalRawDigit.h"
-
-//
-// Only include objects that we would like to be able to put into the event.
-// Do not include the objects they contain internally.
-//
-
-template class art::Wrapper< std::vector<optdata::ADC_Count_t>      >;
-template class art::Wrapper< std::vector< std::pair< optdata::TimeSlice_t, optdata::TimeSlice_t > > >;
-template class art::Wrapper< std::vector<optdata::ChannelData>      >;
-template class art::Wrapper< optdata::ChannelDataGroup              >;
-template class art::Wrapper< std::vector<optdata::ChannelDataGroup> >;
-template class art::Wrapper< std::vector<optdata::FIFOChannel>      >;
-template class art::Wrapper< std::vector<optdata::PMTTrigger>       >;
-template class art::Wrapper< std::vector<optdata::OpticalRawDigit>  >;
