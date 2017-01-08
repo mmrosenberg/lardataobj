549e04ba06f78122a8b94a0c2b148fe204c58cdd
Robert Sulej <robert.sulej@cern.ch>
2017-01-08 12:28:46 -0600

    rename MVAOutput to FeatureVector

--- lardataobj/AnalysisBase/MVAOutput.h
+++ lardataobj/AnalysisBase/MVAOutput.h
@@ -24,41 +24,41 @@ namespace anab {
 /// MVA resuts for a single object, or just a feature vector of size N. Values are saved as 32bit fp's,
 /// this is usually enough for classification purposes and the precision one can expect from MVA algorithms.
 template <size_t N>
-class MVAOutput {
+class FeatureVector {
 public:
 
-    MVAOutput() { }
+    FeatureVector() { }
 
     // MUST UPDATE WHEN CLASS IS CHANGED!
     static short Class_Version() { return 10; }
 
 private:
-    float fOutputs[N]; ///< Vector of MVA output values
+    float fData[N]; ///< Vector of MVA output values
 
 #ifndef __GCCXML__
 public:
 
-    MVAOutput(float init) { set(init); }
-    MVAOutput(std::array<float, N> const & values) { set(values); }
-    MVAOutput(std::array<double, N> const & values) { set(values); }
-    MVAOutput(std::vector<float> const & values) { set(values); }
-    MVAOutput(std::vector<double> const & values) { set(values); }
+    FeatureVector(float init) { set(init); }
+    FeatureVector(std::array<float, N> const & values) { set(values); }
+    FeatureVector(std::array<double, N> const & values) { set(values); }
+    FeatureVector(std::vector<float> const & values) { set(values); }
+    FeatureVector(std::vector<double> const & values) { set(values); }
 
     /// If you really have to use C arrays:
-    MVAOutput(float const * values) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
-    MVAOutput(double const * values) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
+    FeatureVector(float const * values) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
+    FeatureVector(double const * values) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
 
     /// Assignment operators, from the same types as constructors.
-    MVAOutput& operator =(float init) { set(init); return *this; }
-    MVAOutput& operator =(std::array<float, N> const & values) { set(values); return *this; }
-    MVAOutput& operator =(std::array<double, N> const & values) { set(values); return *this; }
-    MVAOutput& operator =(std::vector<float> const & values) { set(values); return *this; }
-    MVAOutput& operator =(std::vector<double> const & values) { set(values); return *this; }
+    FeatureVector& operator =(float init) { set(init); return *this; }
+    FeatureVector& operator =(std::array<float, N> const & values) { set(values); return *this; }
+    FeatureVector& operator =(std::array<double, N> const & values) { set(values); return *this; }
+    FeatureVector& operator =(std::vector<float> const & values) { set(values); return *this; }
+    FeatureVector& operator =(std::vector<double> const & values) { set(values); return *this; }
 
-    friend std::ostream& operator<< (std::ostream &o, MVAOutput const& a)
+    friend std::ostream& operator<< (std::ostream &o, FeatureVector const& a)
     {
-        o << "MVAOutput values:";
-        for (size_t i = 0; i < N; ++i) { o << " " << a.fOutputs[i]; }
+        o << "FeatureVector values:";
+        for (size_t i = 0; i < N; ++i) { o << " " << a.fData[i]; }
         o << std::endl; 
         return o;
     }
@@ -67,31 +67,31 @@ public:
 
     float at(size_t index) const
     {
-        if (index < N) { return fOutputs[index]; }
-        else { throw cet::exception("MVAOutput") << "Index out of range: " << index; }
+        if (index < N) { return fData[index]; }
+        else { throw cet::exception("FeatureVector") << "Index out of range: " << index << std::endl; }
     }
 
-    float operator[] (size_t index) const { return fOutputs[index]; }
+    float operator[] (size_t index) const { return fData[index]; }
 
 private:
 
-    void set(float init) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = init; } }
-    void set(std::array<float, N> const & values) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
-    void set(std::array<double, N> const & values) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
+    void set(float init) { for (size_t i = 0; i < N; ++i) { fData[i] = init; } }
+    void set(std::array<float, N> const & values) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
+    void set(std::array<double, N> const & values) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
     void set(std::vector<float> const & values)
     {
-        if (values.size() == N) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
-        else { throw cet::exception("MVAOutput") << "Expected length: " << N << ", provided: " << values.size(); }
+        if (values.size() == N) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
+        else { throw cet::exception("FeatureVector") << "Expected length: " << N << ", provided: " << values.size() << std::endl; }
     }
     void set(std::vector<double> const & values)
     {
-        if (values.size() == N) { for (size_t i = 0; i < N; ++i) { fOutputs[i] = values[i]; } }
-        else { throw cet::exception("MVAOutput") << "Expected length: " << N << ", provided: " << values.size(); }
+        if (values.size() == N) { for (size_t i = 0; i < N; ++i) { fData[i] = values[i]; } }
+        else { throw cet::exception("FeatureVector") << "Expected length: " << N << ", provided: " << values.size() << std::endl; }
     }
 
 #endif
 
-}; // class MVAOutput
+}; // class FeatureVector
 
 /// MVA results metadata. The idea is to link entire collection of objects to the collection
 /// of MVA results, and add metadata like meaning of columns in MVA results or recommended thresholds
@@ -144,18 +144,18 @@ public:
     void setDataTag(const std::string & tag)
     {
         if (fDataTag.empty()) { fDataTag = tag; }
-        else { throw cet::exception("MVADescription") << "Data tag already assigned: " << fDataTag; }
+        else { throw cet::exception("MVADescription") << "Data tag already assigned: " << fDataTag << std::endl; }
     }
 
     const std::string & outputName(size_t index) const
     {
         if (index < N) { return fOutputNames[index]; }
-        else { throw cet::exception("MVADescription") << "Index out of range: " << index; }
+        else { throw cet::exception("MVADescription") << "Index out of range: " << index << std::endl; }
     }
     void setOutputNames(std::vector< std::string > const & outputNames)
     {
         if (outputNames.size() <= N) { for (size_t i = 0; i < outputNames.size(); ++i) { fOutputNames[i] = outputNames[i]; } }
-        else { throw cet::exception("MVAOutput") << "Expected max length of outputNames: " << N << ", provided: " << outputNames.size(); }
+        else { throw cet::exception("FeatureVector") << "Expected max length of outputNames: " << N << ", provided: " << outputNames.size() << std::endl; }
     }
 
 #endif
