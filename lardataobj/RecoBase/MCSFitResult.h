58ec54a3a72cf92016bdd5807b3158bc1de70e7a
Giuseppe Cerati <cerati@fnal.gov>
2017-05-15 16:08:22 -0500

    default constructror, const vectors, abs in deltaLogLikelihood

--- lardataobj/RecoBase/MCSFitResult.h
+++ lardataobj/RecoBase/MCSFitResult.h
@@ -4,8 +4,8 @@
 namespace recob {
   class MCSFitResult {
   public:
-    MCSFitResult() {}
-  MCSFitResult(int pid, double momFwd, double momFwdUnc, double llhdFwd, double momBwd, double momBwdUnc, double llhdBwd, std::vector<double>& radlengths, std::vector<double>& angles)
+    MCSFitResult()=default;
+    MCSFitResult(int pid, double momFwd, double momFwdUnc, double llhdFwd, double momBwd, double momBwdUnc, double llhdBwd, const std::vector<double>& radlengths, const std::vector<double>& angles)
       : pid_(pid), momFwd_(momFwd), momFwdUnc_(momFwdUnc), llhdFwd_(llhdFwd), momBwd_(momBwd), momBwdUnc_(momBwdUnc), llhdBwd_(llhdBwd), radlengths_(radlengths), angles_(angles) {}
     //
     int    particleIdHyp()     const { return pid_;   }
@@ -22,7 +22,7 @@ namespace recob {
     double bestMomentum()       const { return isBestFwd() ? momFwd_    : momBwd_;   }
     double bestMomUncertainty() const { return isBestFwd() ? momFwdUnc_ : momBwdUnc_;}
     double bestLogLikelihood()  const { return isBestFwd() ? llhdFwd_   : llhdBwd_;  }
-    double deltaLogLikelihood() const { return llhdFwd_-llhdBwd_; }
+    double deltaLogLikelihood() const { return std::abs(llhdFwd_-llhdBwd_); }
     //
   private:
     double pid_;
