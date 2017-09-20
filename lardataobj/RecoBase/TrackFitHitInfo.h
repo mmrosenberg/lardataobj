4899e2bfe6852c6e1bd55b4bc6ad814c2453abca
Giuseppe Cerati <cerati@fnal.gov>
2017-09-19 23:04:57 -0500

    improved documentation

--- lardataobj/RecoBase/TrackFitHitInfo.h
+++ lardataobj/RecoBase/TrackFitHitInfo.h
@@ -8,7 +8,24 @@ namespace recob {
 
   using SVector5     = tracking::SVector5;
   using SMatrixSym55 = tracking::SMatrixSym55;
-  
+
+  /// \file  lardataobj/RecoBase/TrackFitHitInfo.h
+  /// \class recob::TrackFitHitInfo
+  ///
+  /// \brief Object storing per-hit information from a track fit.
+  ///
+  /// \author  G. Cerati (FNAL, MicroBooNE)
+  /// \date    2017
+  /// \version 1.0
+  ///
+  /// This object stores the information related to each hit from a track fit.
+  /// In particular it stores the 1D hit position measurement (TrackFitHitInfo#fHitMeas) and its squared uncertainty (TrackFitHitInfo#fHitMeasErr2),
+  /// the ids of the wire where the hit is found (TrackFitHitInfo#fCryostatId,TrackFitHitInfo#fTpcId,TrackFitHitInfo#fPlaneId,TrackFitHitInfo#fWireId),
+  /// the track parameters (TrackFitHitInfo#fTrackStatePar) and correspoding covariance matrix (TrackFitHitInfo#fTrackStateCov).
+  /// The track parameters need to be defined at the wire location, and, in order to make and unbiased comparison of the hit and track parameters,
+  /// they should be obtained without using this hit in the fit.
+  ///
+
   class TrackFitHitInfo {
   public:
   TrackFitHitInfo(double aHitMeas, double aHitMeasErr2, const SVector5& aTrackStatePar, const SMatrixSym55& aTrackStateCov, const geo::WireID& aWireId)
@@ -18,23 +35,28 @@ namespace recob {
 
     TrackFitHitInfo() {}
     
+    /// hit position measurement
     double hitMeas()     const { return fHitMeas; }
+    /// squared uncertainty of the hit position measurement
     double hitMeasErr2() const { return fHitMeasErr2; }
     
+    /// track parameters
     const SVector5&     trackStatePar() const { return fTrackStatePar; }
+    /// covariance matrix
     const SMatrixSym55& trackStateCov() const { return fTrackStateCov; }
 
+    /// wire id where the hit is located
     geo::WireID WireId() const { return geo::WireID(fCryostatId,fTpcId,fPlaneId,fWireId); }
     
   private:
-    double            fHitMeas;
-    double            fHitMeasErr2;
-    SVector5          fTrackStatePar;
-    SMatrixSym55      fTrackStateCov;
-    unsigned int      fWireId;
-    unsigned int      fPlaneId;
-    unsigned int      fTpcId;
-    unsigned int      fCryostatId;
+    double            fHitMeas;       ///< hit position measurement
+    double            fHitMeasErr2;   ///< squared uncertainty of the hit position measurement
+    SVector5          fTrackStatePar; ///< track parameters
+    SMatrixSym55      fTrackStateCov; ///< covariance matrix
+    unsigned int      fWireId;        ///< wire id where the hit is located
+    unsigned int      fPlaneId;       ///< plane id where the hit is located
+    unsigned int      fTpcId;         ///< tpc id where the hit is located
+    unsigned int      fCryostatId;    ///< cryostat id where the hit is located
   };
   
 }
--- lardataobj/RecoBase/TrackingPlane.h
+++ lardataobj/RecoBase/TrackingPlane.h
@@ -1,34 +1,43 @@
 #ifndef TRACKINGPLANE_H
 #define TRACKINGPLANE_H
 
-/// \brief Class defining a plane for tracking.
-///        It provides various functionalities to convert track parameters and covariance matrices from global to local coordinates.
-///
-/// The plane is constructed from a point in global coordinates, and a direction unit vector orthogonal to the plane.
-/// For instance, they may refer to the position and direction of a trajectory at a given point,
-/// or to the center of a wire and the direction orthogonal to both the wire and drift directions.
-///
-/// The plane and parameters are defined along the lines of trkf::SurfXYZPlane, but with a different notation for the angles
-/// which are now called alpha and beta to avoid confusion with theta and phi in global spherical coordinates:
-/// (alpha, beta) correspond to (alpha := Rotation angle about y'-axis (projected Lorentz angle) = atan2(nx, hypot(ny, nz)),
-///                               beta := Rotation angle about x-axis (wire angle) = atan2(-ny, nz))
-/// The 3D local positions are defined in terms of the unit vectors u,v,w where w is along the normal direction to the plane,
-/// u is perpendicular to w and coplanar to the global x axis and with position sign along the positive sign of x,
-/// v forms a right handed orthonormal basis with u and w.
-/// The 5D track parameters are u,v,du/dw,dv/dw,1/p:
-/// u,v are the local positions, du, dv, and dw are the local directions, and 1/p is the inverse of the track momentum.
-/// The global 6D coordinates are formed by the track position and momentum (or direction) at a given point.
-///
-/// The discussion above refers to a detector with drift direction along the x axis;
-/// this class will have to be extended for a detector with a different drift direction.
-
 #include "lardataobj/RecoBase/TrackingTypes.h"
 
 namespace recob {
   namespace tracking {
 
+    /// \file  lardataobj/RecoBase/TrackingPlane.h
+    /// \class recob::tracking::Plane
+    ///
+    /// \brief Class defining a plane for tracking.
+    ///        It provides various functionalities to convert track parameters and covariance matrices from global to local coordinates.
+    ///
+    /// \author  G. Cerati (FNAL, MicroBooNE)
+    /// \date    2017
+    /// \version 1.0
+    ///
+    /// The plane is constructed from a point in global coordinates, and a direction unit vector orthogonal to the plane.
+    /// For instance, they may refer to the position and direction of a trajectory at a given point,
+    /// or to the center of a wire and the direction orthogonal to both the wire and drift directions.
+    ///
+    /// The plane and parameters are defined along the lines of trkf::SurfXYZPlane, but with a different notation for the angles
+    /// which are now called alpha and beta to avoid confusion with theta and phi in global spherical coordinates:
+    /// (alpha, beta) correspond to (alpha := Rotation angle about y'-axis (projected Lorentz angle) = atan2(nx, hypot(ny, nz)),
+    ///                               beta := Rotation angle about x-axis (wire angle) = atan2(-ny, nz))
+    /// The 3D local positions are defined in terms of the unit vectors u,v,w where w is along the normal direction to the plane,
+    /// u is perpendicular to w and coplanar to the global x axis and with position sign along the positive sign of x,
+    /// v forms a right handed orthonormal basis with u and w.
+    /// The 5D track parameters are u,v,du/dw,dv/dw,1/p:
+    /// u,v are the local positions, du, dv, and dw are the local directions, and 1/p is the inverse of the track momentum.
+    /// The global 6D coordinates are formed by the track position and momentum (or direction) at a given point.
+    ///
+    /// The discussion above refers to a detector with drift direction along the x axis;
+    /// this class will have to be extended for a detector with a different drift direction.
+
     class Plane {
 
+      ///
+      /// Struct caching trigonometric function results.
       struct TrigCache {
       public:
 	TrigCache(const Vector_t& planeDir)
@@ -61,103 +70,52 @@ namespace recob {
       Vector_t const& direction() const { return fPlaneDir; }
 
       //@{
-      /// Function to convert parameters from global to local coordinates.
-      /// Local coordinates are on the plane with origin at planePos and orthogonal to planeDir.
-      /// It is responsibility of the user to make sure the global position lies on the plane
-      static SVector5 Global6DToLocal5DParameters(const SVector6& par6d, const Point_t& planePos, const Vector_t& planeDir) {
-	Plane p(planePos, planeDir); return p.Global6DToLocal5DParameters(par6d);
-      }
+      /// Function to convert parameters from global to local coordinates. Local coordinates are on the plane with origin at planePos and orthogonal to planeDir. It is responsibility of the user to make sure the global position lies on the plane
       inline SVector5 Global6DToLocal5DParameters(const SVector6& par6d) const { return Global6DToLocal5DParameters(par6d, fPlanePos, fPlaneDir, fTrigCache); }
       SVector5 Global6DToLocal5DParameters(const SVector6& par6d, const Point_t& planePos, const Vector_t& planeDir, const TrigCache& trigCache) const;
       //@}
 
       //@{
-      /// Function to convert parameters from local to global coordinates.
-      /// Local coordinates are on the plane with origin at planePos and orthogonal to planeDir.
-      /// trackAlongPlaneDir is as given by trackDir.Dot(planeDir)>0
-      static SVector6 Local5DToGlobal6DParameters(const SVector5& par5d, const Point_t& planePos, const Vector_t& planeDir, bool trackAlongPlaneDir = true) {
-	Plane p(planePos, planeDir); return p.Local5DToGlobal6DParameters(par5d, trackAlongPlaneDir);
-      }
+      /// Function to convert parameters from local to global coordinates. Local coordinates are on the plane with origin at planePos and orthogonal to planeDir. trackAlongPlaneDir is as given by trackDir.Dot(planeDir)>0
       inline SVector6 Local5DToGlobal6DParameters(const SVector5& par5d, bool trackAlongPlaneDir = true) const { return Local5DToGlobal6DParameters(par5d, fPlanePos, fPlaneDir, fTrigCache, trackAlongPlaneDir); }
       SVector6 Local5DToGlobal6DParameters(const SVector5& par5d, const Point_t& planePos, const Vector_t& planeDir, const TrigCache& trigCache, bool trackAlongPlaneDir = true) const;
       //@}
 
       //@{
-      /// Compute the jacobian to translate track covariance from local to global coordinates.
-      /// The track momentum (or direction) is needed to compute the jacobian.
-      /// Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
-      static SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Local5DToGlobal6DJacobian(hasMomentum, trackMomOrDir);
-      }
-      static SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const SVector6& par6d, const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Local5DToGlobal6DJacobian(hasMomentum, Vector_t(par6d[3],par6d[4],par6d[5]));
-      }
+      /// Compute the jacobian to translate track covariance from local to global coordinates. The track momentum (or direction) is needed to compute the jacobian. Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
       inline SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir) const { return Local5DToGlobal6DJacobian(hasMomentum, trackMomOrDir,fPlaneDir,fTrigCache); }
       SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir, const TrigCache& trigCache) const;
       //@}
 
       //@{
-      /// Compute the jacobian to translate track covariance from global to local coordinates.
-      /// The track momentum (or direction) is needed to compute the jacobian.
-      /// Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
-      /// Warning: some information may be lost in degenerate cases, e.g. the unceratinty along z position when converting to a x-y plane (fixed z)
-      static SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Global6DToLocal5DJacobian(hasMomentum, trackMomOrDir);
-      }
-      static SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const SVector6& par6d, const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Global6DToLocal5DJacobian(hasMomentum, Vector_t(par6d[3],par6d[4],par6d[5]));
-      }
+      /// Compute the jacobian to translate track covariance from global to local coordinates. The track momentum (or direction) is needed to compute the jacobian. Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic). Warning: some information may be lost in degenerate cases, e.g. the unceratinty along z position when converting to a x-y plane (fixed z)
       inline SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir) const { return Global6DToLocal5DJacobian(hasMomentum, trackMomOrDir,fPlaneDir,fTrigCache); }
       SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir, const TrigCache& trigCache) const;
       //@}
 
-      //@{
-      /// Translate track covariance from local to global coordinates.
-      /// The track momentum (or direction) is needed to compute the jacobian.
-      /// Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
-      static SMatrixSym66 Local5DToGlobal6DCovariance(SMatrixSym55 cov5d, bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
-	return ROOT::Math::Similarity(Local5DToGlobal6DJacobian(hasMomentum,trackMomOrDir,planeDir),cov5d);
-      }
+      /// Translate track covariance from local to global coordinates. The track momentum (or direction) is needed to compute the jacobian. Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
       inline SMatrixSym66 Local5DToGlobal6DCovariance(SMatrixSym55 cov5d, bool hasMomentum, const Vector_t& trackMomOrDir) const {
 	return Local5DToGlobal6DCovariance(cov5d, hasMomentum,trackMomOrDir,fPlaneDir);
       }
-      //@}
 
-      //@{
-      /// Translate track covariance from global to local coordinates.
-      /// The track momentum (or direction) is needed to compute the jacobian.
-      /// Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic).
-      /// Warning: some information may be lost in degenerate cases, e.g. the unceratinty along z position when converting to a x-y plane (fixed z)
-      static SMatrixSym55 Global6DToLocal5DCovariance(SMatrixSym66 cov6d, bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
-	return ROOT::Math::Similarity(Global6DToLocal5DJacobian(hasMomentum,trackMomOrDir,planeDir),cov6d);
-      }
+      /// Translate track covariance from global to local coordinates. The track momentum (or direction) is needed to compute the jacobian. Local coordinates are on the plane orthogonal to planeDir (it may be the same direction as the momentum, but the function is generic). Warning: some information may be lost in degenerate cases, e.g. the unceratinty along z position when converting to a x-y plane (fixed z)
       inline SMatrixSym55 Global6DToLocal5DCovariance(SMatrixSym66 cov6d, bool hasMomentum, const Vector_t& trackMomOrDir) const {
 	return Global6DToLocal5DCovariance(cov6d,hasMomentum,trackMomOrDir,fPlaneDir);
       }
-      //@}
 
       //@{
-      ///
       /// Calculate rotation matrices from global (x,y,z) to local (u,v,w) coordinates
-      static Rotation_t Global3DToLocal3DRotation(const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Global3DToLocal3DRotation();
-      }
       inline Rotation_t Global3DToLocal3DRotation() const { return Global3DToLocal3DRotation(fPlaneDir,fTrigCache); }
       Rotation_t Global3DToLocal3DRotation(const Vector_t& planeDir, const TrigCache& trigCache) const;
       //@}
 
       //@{
-      ///
       /// Calculate rotation matrices from local (u,v,w) to global (x,y,z) coordinates
-      static Rotation_t Local3DToGlobal3DRotation(const Vector_t& planeDir) {
-	Plane p(Point_t(), planeDir); return p.Local3DToGlobal3DRotation();
-      }
       inline Rotation_t Local3DToGlobal3DRotation() const { return Local3DToGlobal3DRotation(fPlaneDir,fTrigCache); }
       Rotation_t Local3DToGlobal3DRotation(const Vector_t& planeDir, const TrigCache& trigCache) const;
       //@}
 
       //@{
-      ///
       /// Return cached values of trigonometric function for angles defining the plane
       double cosAlpha() const { return fTrigCache.fCosA; }
       double sinAlpha() const { return fTrigCache.fSinA; }
@@ -165,10 +123,51 @@ namespace recob {
       double sinBeta()  const { return fTrigCache.fSinB; }
       //@}
 
+      /// \copydoc Plane::Global6DToLocal5DParameters
+      static SVector5 Global6DToLocal5DParameters(const SVector6& par6d, const Point_t& planePos, const Vector_t& planeDir) {
+	Plane p(planePos, planeDir); return p.Global6DToLocal5DParameters(par6d);
+      }
+      /// \copydoc Plane::Local5DToGlobal6DParameters
+      static SVector6 Local5DToGlobal6DParameters(const SVector5& par5d, const Point_t& planePos, const Vector_t& planeDir, bool trackAlongPlaneDir = true) {
+	Plane p(planePos, planeDir); return p.Local5DToGlobal6DParameters(par5d, trackAlongPlaneDir);
+      }
+      /// \copydoc Plane::Local5DToGlobal6DJacobian
+      static SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Local5DToGlobal6DJacobian(hasMomentum, trackMomOrDir);
+      }
+      /// \copydoc Plane::Local5DToGlobal6DJacobian
+      static SMatrix65 Local5DToGlobal6DJacobian(bool hasMomentum, const SVector6& par6d, const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Local5DToGlobal6DJacobian(hasMomentum, Vector_t(par6d[3],par6d[4],par6d[5]));
+      }
+      /// \copydoc Plane::Global6DToLocal5DJacobian
+      static SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Global6DToLocal5DJacobian(hasMomentum, trackMomOrDir);
+      }
+      /// \copydoc Plane::Global6DToLocal5DJacobian
+      static SMatrix56 Global6DToLocal5DJacobian(bool hasMomentum, const SVector6& par6d, const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Global6DToLocal5DJacobian(hasMomentum, Vector_t(par6d[3],par6d[4],par6d[5]));
+      }
+      /// \copydoc Plane::Local5DToGlobal6DCovariance
+      static SMatrixSym66 Local5DToGlobal6DCovariance(SMatrixSym55 cov5d, bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
+	return ROOT::Math::Similarity(Local5DToGlobal6DJacobian(hasMomentum,trackMomOrDir,planeDir),cov5d);
+      }
+      /// \copydoc Plane::Global6DToLocal5DCovariance
+      static SMatrixSym55 Global6DToLocal5DCovariance(SMatrixSym66 cov6d, bool hasMomentum, const Vector_t& trackMomOrDir, const Vector_t& planeDir) {
+	return ROOT::Math::Similarity(Global6DToLocal5DJacobian(hasMomentum,trackMomOrDir,planeDir),cov6d);
+      }
+      /// \copydoc Plane::Global3DToLocal3DRotation
+      static Rotation_t Global3DToLocal3DRotation(const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Global3DToLocal3DRotation();
+      }
+      /// \copydoc Plane::Local3DToGlobal3DRotation
+      static Rotation_t Local3DToGlobal3DRotation(const Vector_t& planeDir) {
+	Plane p(Point_t(), planeDir); return p.Local3DToGlobal3DRotation();
+      }
+
     private:
-      Point_t fPlanePos;
-      Vector_t fPlaneDir;
-      TrigCache fTrigCache;
+      Point_t   fPlanePos;  ///< Position of a point on the plane
+      Vector_t  fPlaneDir;  ///< Direction vector othogonal to the plane
+      TrigCache fTrigCache; ///< Cached trigonometric function values
     };
 
   }
