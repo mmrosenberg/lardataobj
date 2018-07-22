f1c4537d5c6c2f56c3ce8fe952f6172261b942b4
Jose Soto <j.soto@cern.ch>
2018-07-22 17:37:56 -0500

    edition of SimDriftedElectronCluster

--- lardataobj/Simulation/SimDriftedElectronCluster.h
+++ lardataobj/Simulation/SimDriftedElectronCluster.h
@@ -1,6 +1,6 @@
 //vim: set sw=2 expandtab:
 ////////////////////////////////////////////////////////////////////////
-/// \file  SimDriftedElectronCluster.h
+/// \file  lardataobj/Simulation/SimDriftedElectronCluster.h
 /// \brief contains objects relating to SimDriftedElectronCluster
 ////////////////////////////////////////////////////////////////////////
 
@@ -10,12 +10,6 @@
 // LArSoft includes
 // Define the LArSoft standard geometry types and methods.
 #include "larcoreobj/SimpleTypesAndConstants/geo_vectors.h"
-
-// ROOT includes
-#include "Math/GenVector/Cartesian3D.h"
-#include "Math/GenVector/PositionVector3D.h"
-#include "Math/GenVector/PxPyPzE4D.h"
-#include "Math/GenVector/LorentzVector.h"
 
 // C++ includes
 #include <iostream>
@@ -27,94 +21,52 @@ namespace sim
 
   class SimDriftedElectronCluster
   {
-  public:
+  private:
     using Length_t = double;
-    using Point_t = ROOT::Math::PositionVector3D< ROOT::Math::Cartesian3D<Length_t> >;
+    using StoredPoint_t = geo::GenPoint3DBase_t<double>;
+    using StoredVector_t = geo::GenVector3DBase_t<double>;
 
-    SimDriftedElectronCluster(int ne = 0,
-		     double t = 0.,
-		     Point_t start = {0.,0.,0.},
-		     Point_t end = {0.,0.,0.},
-		     Point_t width = {0.,0.,0.},
-		     double e = 0.,
-		     int id = 0)
-      : NumberOfElectrons(ne)
-      , Time(t)
-      , InitialPosition(start)
-      , FinalPosition(end)
-      , ClusterWidth(width)
-      , Energy(e)
-      , MotherTrackID(id)
+  public:
+
+    SimDriftedElectronCluster() = default;
+
+    SimDriftedElectronCluster(int ne,
+          double t,
+          StoredPoint_t start,
+          StoredPoint_t end,
+          StoredPoint_t width,
+          float e,
+          int id)
+      : fNumberOfElectrons(ne)
+      , fTime(t)
+      , fInitialPosition(start)
+      , fFinalPosition(end)
+      , fClusterWidth(width)
+      , fEnergy(e)
+      , fMotherTrackID(id)
     {}
 
+    int NumberOfElectrons() const { return fNumberOfElectrons; }
+    double Time() const { return fTime; }
+    geo::Point_t InitialPosition() const { return fInitialPosition; }
+    geo::Point_t FinalPosition() const { return fFinalPosition; }
+    geo::Vector_t ClusterWidth() const { return fClusterWidth; }
+    float Energy() const { return fEnergy; }
+    int TrackID() const { return fMotherTrackID; }
 
-#ifndef __GCCXML__
-    // Accessors, hidden from the ROOT dictionary generation.
-    // Note that even if we store a value as float, we return
-    // it as double so the user doesn't have to think about
-    // precision issues.
-
-    int getNumberOfElectrons() const { return NumberOfElectrons; }
-    double getTime() const { return Time; }
-    geo::Point_t getInitialPosition() const { return { InitialPosition.X(), InitialPosition.Y(), InitialPosition.Z() }; }
-    geo::Point_t getFinalPosition() const { return { FinalPosition.X(), FinalPosition.Y(), FinalPosition.Z() }; }
-    geo::Point_t getClusterWidth() const { return { ClusterWidth.X(), ClusterWidth.Y(), ClusterWidth.Z() }; }
-    double getEnergy() const { return Energy; }
-    int getTrackID() const { return MotherTrackID; }
-
-    geo::Length_t FinalPositionX() const { return FinalPosition.X(); }
-    geo::Length_t FinalPositionY() const { return FinalPosition.Y(); }
-    geo::Length_t FinalPositionZ() const { return FinalPosition.Z(); }
-
-
-    bool operator<(const SimDriftedElectronCluster& rhs) const
-    {
-      return NumberOfElectrons < rhs.NumberOfElectrons
-	&& Time < rhs.Time
-	&& InitialPosition.Z() < rhs.InitialPosition.Z()
-	&& InitialPosition.Y() < rhs.InitialPosition.Y()
-	&& InitialPosition.X() < rhs.InitialPosition.X()
-	&& FinalPosition.Z() < rhs.FinalPosition.Z()
-	&& FinalPosition.Y() < rhs.FinalPosition.Y()
-	&& FinalPosition.X() < rhs.FinalPosition.X()
-	&& Energy > rhs.Energy
-	&& MotherTrackID > rhs.MotherTrackID;
-    }
-
-#endif // __GCCXML__
+    geo::Length_t FinalPositionX() const { return fFinalPosition.X(); }
+    geo::Length_t FinalPositionY() const { return fFinalPosition.Y(); }
+    geo::Length_t FinalPositionZ() const { return fFinalPosition.Z(); }
 
   private:
-    // While the accessors above return all values in double
-    // precision, store whatever we can in single precision to save
-    // memory and disk space.
 
-    // There are roughly 7 digits of decimal precision in a float.
-    // This will suffice for energy. A float (as opposed to a double)
-    // can hold a little more than 7 digits of decimal precision. The
-    // smallest position resolution in the simulation is about 0.1mm,
-    // or 10^-4m. With seven digits of precision, that means a float
-    // can be accurate to up to the range of 10^3m. That's why the
-    // definition of our local Point_t (see above) is based on float,
-    // while geo::Point_t is based on double.
-
-    // If the above reasoning is wrong, just change the definition of
-    // Length_t near the top of this file. Of course, also edit these
-    // comments if you do, because you're a good and responsible
-    // programmer.
-
-    // For time, it's possible for long-lived particles like neutrons
-    // to deposit energy after billions of ns. Chances are time cuts
-    // will take care of that, but let's make sure that any overlay studies
-    // won't suffer due to lack of precision.
-
-    int           NumberOfElectrons; //< of ionization electrons
-    double        Time;    //< (ns)
-    Point_t       InitialPosition;     //< positions in (cm)
-    Point_t       FinalPosition;
-    Point_t       ClusterWidth;
-    float         Energy;         //< energy deposition (MeV)
-    int           MotherTrackID;      //< simulation track id
-   // int           MotherTrackID2;      //< simulation track id
+    int            fNumberOfElectrons; //< of ionization electrons
+    double         fTime;    //< (ns)
+    StoredPoint_t  fInitialPosition;     //< positions in (cm)
+    StoredPoint_t  fFinalPosition;
+    StoredVector_t  fClusterWidth;
+    float          fEnergy;         //< energy deposition (MeV)
+    int            fMotherTrackID;      //< simulation track id
   };
 
 } // namespace sim
